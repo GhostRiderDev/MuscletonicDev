6cfae0ce8221af2bb3c3cb1f4c31d48b93c222c1
cos4h <olvadis2004@gmail.com>
2024-03-24 11:44:46 -0500

    configure typeorm datasource postgresql

--- back/src/Config/envs.ts
+++ back/src/Config/envs.ts
@@ -1,4 +1,5 @@
 import dotevn from "dotenv";
 dotevn.config();
 
-export const { SERVER_PORT } = process.env;
+export const { SERVER_PORT, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } =
+  process.env;
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,16 @@
 import { SERVER_PORT } from "./Config/envs";
+import { AppDataSource } from "./data-source";
 import server from "./server";
 
-server.listen(SERVER_PORT, () => {
-  console.log(`Server running on port: ${SERVER_PORT}`);
+server.listen(8888, () => {
+  console.log(`Server running on port http://127.0.0.1:${SERVER_PORT}`);
+  try {
+    connectToDB();
+  } catch (error) {
+    setTimeout(() => connectToDB(), 10000);
+  }
 });
+
+const connectToDB = async () => {
+  await AppDataSource.initialize();
+};
