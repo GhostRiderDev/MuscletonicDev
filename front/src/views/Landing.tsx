e0d01036d849b16701c9a441f0b6e8d2cb8dfe34
cos4h <olvadis2004@gmail.com>
2024-03-29 16:35:36 -0500

    Add new components and update userReducer

--- front/src/views/Landing.tsx
+++ front/src/views/Landing.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "@/components/Auth";
+import Body from "@/components/Body";
 
 function Landing() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<h1> Hello</h1>} />
+        <Route path="/" element={<Body />} />
         <Route path="/home" element={<h1> Hello</h1>} />
         <Route path="/auth" element={<Auth />} />
       </Routes>
